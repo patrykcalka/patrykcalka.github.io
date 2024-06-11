b64be3d1adc4eaf25f6b5169efae8c43531862eb
Patryk Całka <patrykcalka@protonmail.com>
2024-06-11 16:29:02 +0200

    Refactored resx into xml.

--- JavaScript/translation.js
+++ JavaScript/translation.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function loadAndApplyTranslations(language) {
-    const filepath = `../Translations/translation.${language}.resx`;
+    const filepath = `Translations/translation.${language}.xml`;
     console.log(filepath);
     loadXML(filepath, function (xml) {
         const translations = parseResx(xml);
